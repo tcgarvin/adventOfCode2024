3a1fb34a4b84b1196cb1da2050476501c7bb3537
Tim Garvin <tcgarvin@gmail.com>
2024-12-06 12:55:23 -0500

    Abstract a bit for part 2

--- 06/06.py
+++ 06/06.py
@@ -8,26 +8,46 @@ def get_puzzle_input():
     with open("input.txt") as input_txt:
         return grid_from_input_txt(input_txt.read(), "O")
 
-def solve_part_1(grid: Grid):
-    location = puzzle_input.find_one("^")
-    rotation = cycle([NORTH, EAST, SOUTH, WEST])  # Note, Starting with NORTH
-    direction = next(rotation)
-    while grid[location] != "O":
-        grid[location] = "X"
-        while grid[location + direction] == "#":
-            direction = next(rotation)
-        location += direction
+def guard_status(grid:Grid, location, direction, starting_location, starting_direction):
+    if grid[location] == "O":
+        return "off-grid"
+    if location == starting_location and direction == starting_direction:
+        return "loop"
+    return "live"
 
+def traverse(grid:Grid, mark=None):
+    starting_location = grid.find_one("^")
+    rotation = cycle([NORTH, EAST, SOUTH, WEST])  # Note, Starting with NORTH
+    starting_direction = next(rotation)
+
+    location = starting_location
+    direction = starting_direction
+    status = "live"
+    while status == "live":
+        if mark is not None:
+            grid[location] = mark
+        if grid[location + direction] == "#":
+            direction = next(rotation)
+            status = guard_status(grid, location, direction, starting_location, starting_direction)
+
+        location += direction
+        status = guard_status(grid, location, direction, starting_location, starting_direction)
+
+    return status
+
+def solve_part_1(grid: Grid):
+    traverse(grid, mark="X")
+
+    print(len(grid.grid))
     return len(list(grid.find("X")))
 
-def solve_part_2(puzzle_input):
+def solve_part_2(grid:Grid):
+
     return ""
 
 if __name__ == "__main__":
-    puzzle_input = get_puzzle_input()
-
-    answer_1 = solve_part_1(puzzle_input)
+    answer_1 = solve_part_1(get_puzzle_input())
     print(f"Part 1: {answer_1}")
 
-    answer_2 = solve_part_2(puzzle_input)
+    answer_2 = solve_part_2(get_puzzle_input())
     print(f"Part 2: {answer_2}")

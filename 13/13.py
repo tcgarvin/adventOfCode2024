bffe78ade135321f3562b1cb016bc82313bb256a
Tim Garvin <tcgarvin@gmail.com>
2024-12-14 22:22:59 -0500

    Day 13 Part 2

--- 13/13.py
+++ 13/13.py
@@ -2,9 +2,9 @@ import argparse
 import re
 import math
 from typing import NamedTuple
-from rich import print
 
-from grid import Vector
+import numpy as np
+from rich import print
 
 # Regex courtesy of chatgpt
 PATTERN = re.compile(r"""
@@ -23,76 +23,45 @@ def get_puzzle_input(use_example=False):
             puzzle_input.append(MachineInfo(*map(int, match.groups())))
     return puzzle_input
 
-def cheapest_win(machine: MachineInfo) -> int:
-    # Kinda outside the scope of the vector class, but should be fine.
-    a = Vector(machine.ax, machine.ay)
-    b = Vector(machine.bx, machine.by)
-    p = Vector(machine.px, machine.py)
+def cheapest_win(machine: MachineInfo, max_pushes=False, correction=0) -> int:
+    px = machine.px + correction
+    py = machine.py + correction
+    # Ax = b 
+    np_A = np.array([[machine.ax, machine.bx], [machine.ay, machine.by]])
+    np_b = np.array([px, py])
+    raw_solution = np.linalg.solve(np_A, np_b)
+    solution = raw_solution.round()
 
-    # Hypothesis: The "cheapest", really just means the only, so long as A and B are not scalar multiples of eachother.
-    for i in range(10):
-        if a * i == b or b * i == a:
-            print(f"{machine} has scalar multiple buttons.")
+    # Validate solution.  We expect rounding into make bad solutions look checkable, but they should fail.
+    a,b = int(solution[0]), int(solution[1])
+    cost = 0
+    if a < 0 or b < 0:
+        cost = 0
 
-    # We're going to try to change the basis of the coordinate system so that p is defined in terms of A and B.
-    # 1. Get the unit vector of A.
-    print(f"A: {a}")
-    print(f"B: {b}")
-    print(f"P: {p}")
-    a_unit = a.unit()
-    print(f"a_unit: {a_unit}")
+    elif max_pushes is not False and (a > max_pushes or b > max_pushes):
+        cost = 0
 
-    # 2. Get the projection of b onto a.
-    b_dot_a = Vector.dot(b, a_unit)
-    print(f"b_dot_a: {b_dot_a}")
-    b_onto_a = a_unit * b_dot_a
-    print(f"b_onto_a: {b_onto_a}")
+    elif a * machine.ax + b * machine.bx == px and a * machine.ay + b * machine.by == py:
+        cost = 3 * a + b
 
-    # 3. Get B_orthogonal, and its unit.  I feel like I could take a shortcut
-    # here, just by using the unit vector of A and taking an educated guess on
-    # whether to be right-handed or left-handed, but this feels slightly more
-    # scientific.
-    b_orth = b - b_onto_a
-    b_unit = b_orth.unit()
-    print(f"b_unit: {b_unit}")
-
-    # 4. Get the projection of p onto a and b.
-    p_onto_a = Vector.dot(p, a_unit)
-    p_onto_b = Vector.dot(p, b_unit)
-    print(f"p_onto_a: {p_onto_a}")
-    print(f"p_onto_b: {p_onto_b}")
-    new_p = Vector(p_onto_a, p_onto_b)
-    print(f"new_p: {new_p}")
-
-    #5. Let's see if we can get the coordinates of p in terms of a and b.
-    a_count = p.i / abs(a)
-    b_count = p.j / abs(b)
-
-    print(f"a_count: {a_count}, b_count: {b_count}")
-
-    if a_count < 0 or b_count < 0:
-        print(f"{machine} has no solution.")
-        return 0
-
-    for a_i in range(math.floor(a_count), math.ceil(a_count) + 1):
-        for b_i in range(math.floor(b_count), math.ceil(b_count) + 1):
-            if a * a_i + b * b_i == p:
-                print(f"{machine} has solution: {a_i}, {b_i}")
-                return 3 * a_i + b_i
-
-    print(f"{machine} has no solution.")
-    return 0
+    #print(machine)
+    #print(raw_solution, solution, (a,b), cost)
+    return cost
 
 
 def solve_part_1(machines: list[MachineInfo]):
     total_min_cost = 0
     for machine in machines:
-        total_min_cost += cheapest_win(machine)
+        total_min_cost += cheapest_win(machine, max_pushes=100)
 
     return total_min_cost
 
-def solve_part_2(puzzle_input):
-    return ""
+def solve_part_2(machines: list[MachineInfo]):
+    total_min_cost = 0
+    for machine in machines:
+        total_min_cost += cheapest_win(machine, correction=10000000000000)
+
+    return total_min_cost
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()

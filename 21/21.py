1691388ae6203517f9274e7dbf898d973e544f67
Tim Garvin <tcgarvin@gmail.com>
2024-12-26 19:58:40 -0500

    Run black
    Deciding if I should throw this out or not

--- 21/21.py
+++ 21/21.py
@@ -2,7 +2,18 @@ import argparse
 from itertools import permutations
 from rich import print
 
-from grid import Grid, Vector, get_vector, NORTH, SOUTH, EAST, WEST, grid_from_input_txt, print_grid
+from grid import (
+    Grid,
+    Vector,
+    get_vector,
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+    grid_from_input_txt,
+    print_grid,
+)
+
 
 def get_puzzle_input(use_example=False):
     input_filename = "example.txt" if use_example else "input.txt"
@@ -19,18 +30,27 @@ def get_keypad() -> Grid:
     keypad.traverse_order = [NORTH, EAST, SOUTH, WEST]
     return keypad
 
+
 def get_remote_control() -> Grid:
     grid_txt = "\n".join([" ^A", "<v>"])
     remote_control = grid_from_input_txt(grid_txt)
     remote_control.traverse_order = [SOUTH, EAST, NORTH, WEST]
     return remote_control
 
-def is_manhattan_component(component:Vector, whole:Vector):
+
+def is_manhattan_component(component: Vector, whole: Vector):
     # Assumes component is a unit vector north south east or west
-    return (component.j > 0 and whole.j > 0 or
-            component.j < 0 and whole.j < 0 or 
-            component.i > 0 and whole.i > 0 or
-            component.i < 0 and whole.i < 0)
+    return (
+        component.j > 0
+        and whole.j > 0
+        or component.j < 0
+        and whole.j < 0
+        or component.i > 0
+        and whole.i > 0
+        or component.i < 0
+        and whole.i < 0
+    )
+
 
 STEP_CODE_MAP = {
     NORTH: "^",
@@ -39,7 +59,8 @@ STEP_CODE_MAP = {
     WEST: "<",
 }
 
-def plan_code(code:str, grid:Grid, sub_levels=0):
+
+def plan_code(code: str, grid: Grid, sub_levels=0):
     candidate_remote_control = get_remote_control()
     plan = ""
     best_sub_plan_length = 0
@@ -72,14 +93,21 @@ def plan_code(code:str, grid:Grid, sub_levels=0):
                         break
                 if not valid:
                     continue
-                candidate_char_plan = "".join(STEP_CODE_MAP[step] for step in candidate_char_plan) + "A"
-                _, char_sub_plan_length = plan_code(candidate_char_plan, candidate_remote_control, sub_levels=sub_levels-1)
+                candidate_char_plan = (
+                    "".join(STEP_CODE_MAP[step] for step in candidate_char_plan) + "A"
+                )
+                _, char_sub_plan_length = plan_code(
+                    candidate_char_plan,
+                    candidate_remote_control,
+                    sub_levels=sub_levels - 1,
+                )
                 if char_sub_plan_length < best_char_sub_plan_length:
                     best_char_sub_plan_length = char_sub_plan_length
                     char_plan = candidate_char_plan
 
         plan += char_plan
         best_sub_plan_length += best_char_sub_plan_length
+        start_location = target_location
 
     return plan, best_sub_plan_length
 
@@ -104,11 +132,13 @@ def solve_part_1(puzzle_input):
         plan = solve_code_1(code)
         print(f"{code}: len(plan) * int(code[:-1]): {len(plan)} * {int(code[:-1])}")
         complexity_sum += len(plan) * int(code[:-1])
-        
+
     return complexity_sum
+
 
 def solve_part_2(puzzle_input):
     return ""
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
